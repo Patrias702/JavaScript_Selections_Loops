57e998dabf3e0c76989f3ccdace908f6a97a6c7b
DJ Patrias <Patrias21@gmail.com>
2023-05-01 18:01:52 -0700

    Fixed Backtick Errors And (Let) Errors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // Exercise 1: Ignore Even
 
 
-for (let i = 0; i < 100; i++){
+for (i = 0; i < 100; i++){
     if (i % 2 == 0){
         continue;
     } else{
@@ -16,7 +16,7 @@ for (let i = 0; i < 100; i++){
 
 // Exercise 2: FIZZBUZZ
 
-for (let i = 1; i < 100; i++) {
+for ( i = 1; i < 100; i++) {
     if (i % 3 == 0 && i % 5 == 0){
         console.log("FIZZBUZZ");
     }else{
@@ -66,7 +66,7 @@ while ( i <= 100){
         output += "BUZZ";
     }
 
-    console.log('${i} $ {output}');
+    console.log(`${i} $ {output}`);
 
 
     i++;
@@ -88,7 +88,7 @@ do {
     }
 
 
-    console.log('${x} ${output}');
+    console.log(`${x} ${output}`);
 
     x++;
 } while (x <= 100);
@@ -101,15 +101,15 @@ do {
 let NumberToFind = Math.round((Math.random() * 500)); // creates a random number between 0 and 500
 let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
 
-for (let i = 1; i <= n; i++){
+for (i = 1; i <= n; i++){
     if (i == NumberToFind){
-    console.log(' Found ${numberToFind}!');
+    console.log(`Found ${NumberToFind}`);
     break;
  }
 
 
  if (i == n) {
-  console.log('Did not find ${numberToFind} within.... 1-${n}.. ');
+  console.log(`Did not find ${numberToFind} within.... 1-${n}.. `);
  }
 }
 
@@ -122,7 +122,7 @@ let end = Math.round(Math.random() * (1000 - 1) + 1);
 let start = Math.round(Math.random() * (10 - 1) + 1);
 
 
-for (let i = start; i <= end; i++){
+for (i = start; i <= end; i++){
     let output ="";
 
     if (i % fizzDivisor == 0 ){
@@ -133,7 +133,7 @@ for (let i = start; i <= end; i++){
         output += "BUZZ"
     }
 
-    console.log('${i} ${output}');
+    console.log(`${i} ${output}`);
 }
 
 
